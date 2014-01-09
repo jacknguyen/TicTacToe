f14178284467fa404e76b12abb47845106b63c11
Jack Nguyen <nguyen.jack@gmail.com>
2014-01-09 15:03:25 -0800

    Initial commit

---
+++ js/application-ck.js
@@ -0,0 +1,2 @@
+//loads on site load
+body.onload=function(){function n(){}var e=document.getElementsByClassName("box"),t=document.body.select};
---
+++ js/application.js
@@ -0,0 +1,11 @@
+//loads on site load
+body.onload = function () {
+	var a = document.getElementsByClassName('box');
+	var select = document.body.select;
+
+	//on click remove boxInit and add player click image
+	function changeClass() {
+	//document.getElementById('aa').className = document.getElementById('aa').className.replace('boxInit');
+	//document.getElementById('ac').className+=" p2Click";
+	}
+}
