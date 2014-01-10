5a067bcf6df6a74404872a5d853d5e52b6756a2e
Jack Nguyen <nguyen.jack@gmail.com>
2014-01-09 20:29:13 -0800

    Click function working through JavaScript

--- js/application.js
+++ js/application.js
@@ -1,20 +1,27 @@
-//on click remove boxInit and add player click image
-var a = document.getElementsByClassName('box');
-
-function changeClass() {
-	console.log('clicked');
-	document.getElementsByClassName('gameboard').className = document.getElementById('aa').className.replace('boxInit');
-	document.getElementsByClassName('gameboard').className+=" p1Click";
-
-	for (var i in a ) {
-
-	}
-}
-
-
-
 //loads on site load
 window.onload = function () {
-	// document.getElementsByClassName('box').adEventListener('click',console.log('click'));
-	document.getElementsByClassName('box').on('click', changeClass);
+	var a = document.getElementsByClassName('box');
+	var playerTurn = 1;
+
+	//this does the clicks
+	for (var i=0; i<a.length; i++){
+		a[i].onclick = function() {
+			if (this.classList.contains('p1Click' || 'p2Click')) {
+				//do nothing
+			}else{
+				if (playerTurn==1) {
+					//player 1 click
+					this.className = this.className.replace('boxInit');
+					this.className = this.className += " p1Click";
+					playerTurn++;
+				}else{
+					//player 2 click
+					this.className = this.className.replace('boxInit');
+					this.className = this.className += " p2Click";
+					playerTurn--;
+				}
+			}
+		}
+	}
+
 };
