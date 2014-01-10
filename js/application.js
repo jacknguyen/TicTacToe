79bfa883c99c6485048f23d5f61ea43319bb6843
Jack Nguyen <nguyen.jack@gmail.com>
2014-01-09 18:20:11 -0800

    first commit

--- js/application.js
+++ js/application.js
@@ -1,11 +1,20 @@
-//loads on site load
-body.onload = function () {
-	var a = document.getElementsByClassName('box');
-	var select = document.body.select;
+//on click remove boxInit and add player click image
+var a = document.getElementsByClassName('box');
 
-	//on click remove boxInit and add player click image
-	function changeClass() {
-	//document.getElementById('aa').className = document.getElementById('aa').className.replace('boxInit');
-	//document.getElementById('ac').className+=" p2Click";
+function changeClass() {
+	console.log('clicked');
+	document.getElementsByClassName('gameboard').className = document.getElementById('aa').className.replace('boxInit');
+	document.getElementsByClassName('gameboard').className+=" p1Click";
+
+	for (var i in a ) {
+
 	}
 }
+
+
+
+//loads on site load
+window.onload = function () {
+	// document.getElementsByClassName('box').adEventListener('click',console.log('click'));
+	document.getElementsByClassName('box').on('click', changeClass);
+};
