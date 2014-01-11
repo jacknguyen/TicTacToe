4cb6a783b7d658017ad23d7541dbd48b8aa48505
Jack Nguyen <nguyen.jack@gmail.com>
2014-01-10 18:16:17 -0800

    add reset function on JS

--- js/application-ck.js
+++
@@ -1,2 +0,0 @@
-//loads on site load
-body.onload=function(){function n(){}var e=document.getElementsByClassName("box"),t=document.body.select};
--- js/application.js
+++ js/application.js
@@ -1,11 +1,14 @@
 //loads on site load
 window.onload = function () {
-	var a = document.getElementsByClassName('box');
+	var box = document.getElementsByClassName('box');
 	var playerTurn = 1;
+	var move = 0;
+	var resetButton = document.getElementById('reset');
+
 
 	//this does the clicks
-	for (var i=0; i<a.length; i++){
-		a[i].onclick = function() {
+	for (var i=0; i<box.length; i++){
+		box[i].onclick = function() {
 			if (this.classList.contains('p1Click' || 'p2Click')) {
 				//do nothing
 			}else{
@@ -14,13 +17,34 @@ window.onload = function () {
 					this.className = this.className.replace('boxInit');
 					this.className = this.className += " p1Click";
 					playerTurn++;
+					move++;
+						if (resetButton.innerHTML == "Start") {
+							resetButton.innerHTML = "Reset";
+						}
 				}else{
 					//player 2 click
 					this.className = this.className.replace('boxInit');
 					this.className = this.className += " p2Click";
 					playerTurn--;
+					move++;
 				}
 			}
+		}
+		console.log(move); //move print test
+	}
+
+	//reset game
+	document.getElementById('reset').onclick = function() {
+		resetButton.innerHTML = "Start"; // sets reset button to start
+		//resets class names
+		for (var i=0; i<box.length; i++){
+
+			box[i].className = box[i].className.replace('p1Click');
+			box[i].className = box[i].className.replace('p2Click');
+			box[i].className = box[i].className += " boxInit";
+			move = 0;
+			playerTurn = 1;
+
 		}
 	}
 
